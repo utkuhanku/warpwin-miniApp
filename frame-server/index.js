937ad8ee95044409b1c1f6bd95935af49c6436c3
Utkuhan Kocaturk <utkuhank@hotmail.com>
2025-05-02 19:23:09 +0300

    CORS desteği eklendi

--- frame-server/index.js
+++ frame-server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const { createCanvas } = require('canvas');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Oyun durumu (8x8 tahta)
 let gameState = {
